7ab2ae0400108d618777cab7ada16e478f153571
ScriptFindr <scriptfindr@gmail.com>
2016-06-23 17:42:16 +0200

    Update Extension.js

--- Extension.js
+++ Extension.js
@@ -1,3 +1,23 @@
-var Extension = (function(exports) {
-  return exports;
-})({});
+var Extension = (function() {
+  return {
+    id: 'markdown-',
+    comments: {
+      pattern: /\[comment\]\:(\s){0,}\#(\s){0,}\(([\s\S]*?)\)/gm,
+      remove: /(\[comment\]\:(\s){0,}\#(\s){0,}\(|\)$)/gm,
+      wrap: '<span class="markdown-hide">$&</span>'
+    },
+    escapes: {
+      less: {
+        pattern: /\</gm,
+        replace: '&lt;'
+      },
+      and: {
+        pattern: /\&/gm,
+        replace: '&amp;'
+      }
+    },
+    deadspace: {
+      pattern: /(^[\s\n]+)/gm
+    }
+  };
+})();
